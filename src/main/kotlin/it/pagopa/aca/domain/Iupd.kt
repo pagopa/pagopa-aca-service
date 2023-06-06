0d697af251278f3e53818534a6cbb308c9cf5622
Fernando-Granato <fernando.granato@pagopa.it>
2023-06-06 16:59:24 +0200

    feat: added iupd record

---
+++ src/main/kotlin/it/pagopa/aca/domain/Iupd.kt
@@ -0,0 +1,15 @@
+package it.pagopa.aca.domain
+
+@JvmRecord
+data class Iupd(val fiscalCode: String, val iuv: String) {
+    
+    fun value(): String {
+        return StringBuilder()
+            .append("ACA")
+            .append("_")
+            .append(fiscalCode)
+            .append("_")
+            .append(iuv)
+            .toString()
+    }
+}
