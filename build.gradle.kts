9590fb1348518db12344b3c1a3a00bd2f1c8f36e
Fernando-Granato <fernando.granato@pagopa.it>
2023-06-18 15:56:51 +0200

    fix:restore main branch for gpd

--- build.gradle.kts
+++ build.gradle.kts
@@ -166,7 +166,7 @@ tasks.register("apiConfigAPI", GenerateTask::class.java) {
 tasks.register("gpdAPI", GenerateTask::class.java) {
   generatorName.set("java")
   remoteInputSpec.set(
-    "https://raw.githubusercontent.com/pagopa/pagopa-infra/fix-gpd-openapi/src/domains/gps-app/api/gpd_api/debt-position-services/v1/_openapi.json.tpl"
+    "https://raw.githubusercontent.com/pagopa/pagopa-infra/main/src/domains/gps-app/api/gpd_api/debt-position-services/v1/_openapi.json.tpl"
   )
   outputDir.set("$buildDir/generated")
   apiPackage.set("it.pagopa.generated.gpd.api")
