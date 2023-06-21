ef54728ae67212d9a4ac087d3bed174b1b41b874
Fernando-Granato <fernando.granato@pagopa.it>
2023-06-21 15:45:49 +0200

    feat:added api implementation and utils class

--- src/main/kotlin/it/pagopa/aca/services/AcaService.kt
+++ src/main/kotlin/it/pagopa/aca/services/AcaService.kt
@@ -1,16 +1,100 @@
 package it.pagopa.aca.services
 
+import it.pagopa.aca.client.GpdClient
+import it.pagopa.aca.client.IbansClient
+import it.pagopa.aca.domain.Iupd
+import it.pagopa.aca.exceptions.RestApiException
+import it.pagopa.aca.utils.AcaUtils
 import it.pagopa.generated.aca.model.NewDebtPositionRequestDto
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
 
 @Service
-class AcaService {
+class AcaService(
+    @Autowired private val gpdClient: GpdClient,
+    @Autowired private val ibansClient: IbansClient,
+    @Autowired private val acaUtils: AcaUtils
+) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     suspend fun handleDebitPosition(newDebtPositionRequestDto: NewDebtPositionRequestDto) {
-        // TODO add implementation here
-        logger.info("Handle debit position for amount ${newDebtPositionRequestDto.amount}")
+        logger.info(
+            "Handle debit position for amount: ${newDebtPositionRequestDto.amount} and iuv: ${newDebtPositionRequestDto.iuv}"
+        )
+        val iupd = Iupd(newDebtPositionRequestDto.entityFiscalCode, newDebtPositionRequestDto.iuv)
+        val entityFiscalCode = newDebtPositionRequestDto.entityFiscalCode
+        gpdClient
+            .getDebtPosition(entityFiscalCode, iupd.value())
+            .filterWhen { oldDebtPosition ->
+                if (oldDebtPosition !== null) {
+                    Mono.just(false)
+                } else {
+                    if (acaUtils.checkInvalidateAmount(newDebtPositionRequestDto.amount)) {
+                        Mono.error(
+                            RestApiException(
+                                HttpStatus.BAD_REQUEST,
+                                "Unauthorized action",
+                                "Amount not compatible with the creation request"
+                            )
+                        )
+                    } else {
+                        logger.info("Create new debit position with iupd: ${iupd.value()}")
+                        ibansClient
+                            .getIban(entityFiscalCode, "")
+                            .map {
+                                acaUtils.newDebitPositionObject(
+                                    newDebtPositionRequestDto,
+                                    iupd,
+                                    it.first,
+                                    it.second
+                                )
+                            }
+                            .flatMap { newDebitPosition ->
+                                gpdClient.createDebtPosition(entityFiscalCode, newDebitPosition)
+                            }
+                        Mono.just(true)
+                    }
+                }
+            }
+            .filter { acaUtils.checkStatus(it.status) }
+            .switchIfEmpty(
+                Mono.error(
+                    RestApiException(
+                        HttpStatus.CONFLICT,
+                        "Unauthorized action",
+                        "Unauthorized action on debt position with iuv: ${newDebtPositionRequestDto.iuv}"
+                    )
+                )
+            )
+            .flatMap { oldDebitPosition ->
+                if (acaUtils.checkInvalidateAmount(newDebtPositionRequestDto.amount)) {
+                    logger.info("Invalidate debit position with iupd: ${iupd.value()}")
+                    gpdClient.invalidateDebtPosition(entityFiscalCode, iupd.value())
+                } else {
+                    logger.info("Update debit position with iupd: ${iupd.value()}")
+                    ibansClient
+                        .getIban(entityFiscalCode, "")
+                        .map {
+                            acaUtils.updateOldDebitPositionObject(
+                                oldDebitPosition,
+                                newDebtPositionRequestDto,
+                                iupd,
+                                it.second
+                            )
+                        }
+                        .flatMap { newDebitPosition ->
+                            gpdClient.updateDebtPosition(
+                                entityFiscalCode,
+                                iupd.value(),
+                                newDebitPosition
+                            )
+                        }
+                }
+            }
     }
 }
---
+++ src/main/kotlin/it/pagopa/aca/utils/AcaUtils.kt
@@ -0,0 +1,106 @@
+package it.pagopa.aca.utils
+
+import it.pagopa.aca.domain.Iupd
+import it.pagopa.generated.aca.model.NewDebtPositionRequestDto
+import it.pagopa.generated.gpd.model.PaymentOptionModelDto
+import it.pagopa.generated.gpd.model.PaymentPositionModelBaseResponseDto
+import it.pagopa.generated.gpd.model.PaymentPositionModelDto
+import it.pagopa.generated.gpd.model.TransferModelDto
+import org.springframework.stereotype.Component
+
+@Component
+class AcaUtils {
+
+    companion object {
+        const val STAND_IN_CONSTANT: String = "STAND-IN"
+    }
+    fun checkStatus(status: PaymentPositionModelBaseResponseDto.StatusEnum?): Boolean {
+        return status !== PaymentPositionModelBaseResponseDto.StatusEnum.DRAFT &&
+            status !== PaymentPositionModelBaseResponseDto.StatusEnum.PUBLISHED &&
+            status !== PaymentPositionModelBaseResponseDto.StatusEnum.VALID
+    }
+
+    fun checkInvalidateAmount(amount: Int): Boolean {
+        return amount == 0
+    }
+
+    fun newDebitPositionObject(
+        newDebtPositionRequestDto: NewDebtPositionRequestDto,
+        iupd: Iupd,
+        iban: String,
+        companyName: String?
+    ): PaymentPositionModelDto {
+        return PaymentPositionModelDto()
+            .iupd(iupd.value())
+            .fiscalCode(newDebtPositionRequestDto.entityFiscalCode)
+            .type(
+                PaymentPositionModelDto.TypeEnum.valueOf(newDebtPositionRequestDto.entityType.value)
+            )
+            .fullName(newDebtPositionRequestDto.entityFullName)
+            .companyName(companyName)
+            .paymentOption(
+                listOf(
+                    PaymentOptionModelDto()
+                        .iuv(newDebtPositionRequestDto.iuv)
+                        .amount(newDebtPositionRequestDto.amount.toLong())
+                        .description(newDebtPositionRequestDto.description)
+                        .isPartialPayment(false)
+                        .dueDate(newDebtPositionRequestDto.expirationDate)
+                        .transfer(
+                            listOf(
+                                TransferModelDto()
+                                    .amount(newDebtPositionRequestDto.amount.toLong())
+                                    .idTransfer(TransferModelDto.IdTransferEnum._1)
+                                    .category(STAND_IN_CONSTANT)
+                                    .iban(iban)
+                            )
+                        )
+                )
+            )
+    }
+
+    fun updateOldDebitPositionObject(
+        oldDebitPosition: PaymentPositionModelBaseResponseDto,
+        newDebtPositionRequestDto: NewDebtPositionRequestDto,
+        iupd: Iupd,
+        companyName: String?
+    ): PaymentPositionModelDto {
+        return PaymentPositionModelDto()
+            .fiscalCode(newDebtPositionRequestDto.entityFiscalCode)
+            .officeName(oldDebitPosition.officeName)
+            .validityDate(null)
+            .type(
+                PaymentPositionModelDto.TypeEnum.valueOf(newDebtPositionRequestDto.entityType.value)
+            )
+            .fullName(newDebtPositionRequestDto.entityFullName)
+            .companyName(companyName)
+            .paymentOption(
+                oldDebitPosition.paymentOption
+                    ?.stream()
+                    ?.map {
+                        PaymentOptionModelDto()
+                            .iuv(newDebtPositionRequestDto.iuv)
+                            .amount(newDebtPositionRequestDto.amount.toLong())
+                            .description(newDebtPositionRequestDto.description)
+                            .dueDate(newDebtPositionRequestDto.expirationDate)
+                            .transfer(
+                                it.transfer
+                                    ?.stream()
+                                    ?.map { transfer ->
+                                        TransferModelDto()
+                                            .amount(newDebtPositionRequestDto.amount.toLong())
+                                            .idTransfer(
+                                                transfer.idTransfer?.let { it1 ->
+                                                    TransferModelDto.IdTransferEnum.valueOf(it1)
+                                                }
+                                            )
+                                            .category(transfer.category)
+                                            .iban(transfer.iban)
+                                    }
+                                    ?.toList()
+                            )
+                    }
+                    ?.toList()
+            )
+    }
+}
