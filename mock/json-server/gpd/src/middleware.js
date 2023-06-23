403bd05c30930d88abfe371f7ad03f1196ddbd12
Pietro Tota <pietro.tota@pagopa.it>
2023-06-23 16:00:33 +0200

    feat(mock) - add request timestamp

--- mock/json-server/gpd/src/middleware.js
+++ mock/json-server/gpd/src/middleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  console.log(`Received request: ${req.method} - ${JSON.stringify(req.body)}`)
+  console.log(`${new Date().toISOString()} - Received request: ${req.method} ${req.path} - Body:  ${JSON.stringify(req.body)}`)
   req.method = 'GET'
   next()
 }
