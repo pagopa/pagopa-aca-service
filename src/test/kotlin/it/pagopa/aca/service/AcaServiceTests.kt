589e0b31e34d934c529564cd3bb8b99c35367ea5
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-05-20 11:56:59 +0200

    [PAGOPA-1711] test: Add update unit test cases

--- src/test/kotlin/it/pagopa/aca/service/AcaServiceTests.kt
+++ src/test/kotlin/it/pagopa/aca/service/AcaServiceTests.kt
@@ -173,7 +173,7 @@ class AcaServiceTests {
     }
 
     @Test
-    fun `update position successfully`() = runTest {
+    fun `update position (no iban, no postalIban) successfully`() = runTest {
         val requestCreatePosition = AcaTestUtils.createPositionRequestBody(iupd, 10)
         val responseGetPosition =
             AcaTestUtils.responseGetPosition(
@@ -214,6 +214,122 @@ class AcaServiceTests {
                     iupd,
                     ibanTestUpdate,
                     companyName
+                )
+            )
+        verify(gpdClient, Mockito.times(0)).createDebtPosition(any(), any(), anyOrNull())
+        verify(gpdClient, Mockito.times(0))
+            .invalidateDebtPosition(requestCreatePosition.paFiscalCode, iupd.value())
+        assertEquals(
+            ibanTestUpdate,
+            paymentPositionModelDtoCaptor.firstValue.paymentOption?.get(0)?.transfer?.get(0)?.iban
+        )
+    }
+
+    @Test
+    fun `update position (no postalIban) successfully`() = runTest {
+        val requestCreatePosition =
+            AcaTestUtils.createPositionRequestBody(
+                iupd,
+                10,
+                ibanTestUpdate,
+                postalIban = null,
+                true
+            )
+        val responseGetPosition =
+            AcaTestUtils.responseGetPosition(
+                iupd,
+                10,
+                ibanTest,
+                PaymentPositionModelBaseResponseDto.StatusEnum.DRAFT
+            )
+        val responseCreate = AcaTestUtils.debitPositionModelResponse(iupd)
+        /* preconditions */
+        given(gpdClient.getDebtPosition(any(), any(), anyOrNull()))
+            .willReturn(Mono.just(responseGetPosition))
+        given(ibansClient.getIban(any(), any()))
+            .willReturn(Mono.just(Pair(ibanTestUpdate, companyName)))
+        given(
+                gpdClient.updateDebtPosition(
+                    any(),
+                    any(),
+                    paymentPositionModelDtoCaptor.capture(),
+                    anyOrNull()
+                )
+            )
+            .willReturn(Mono.just(responseCreate))
+        /* tests */
+        acaService.handleDebitPosition(requestCreatePosition)
+        /* Asserts */
+        verify(gpdClient, Mockito.times(1))
+            .getDebtPosition(requestCreatePosition.paFiscalCode, iupd.value())
+        verify(ibansClient, Mockito.times(0))
+            .getIban(eq(requestCreatePosition.paFiscalCode), anyOrNull())
+        verify(gpdClient, Mockito.times(1))
+            .updateDebtPosition(
+                requestCreatePosition.paFiscalCode,
+                iupd.value(),
+                acaUtils.updateOldDebitPositionObject(
+                    responseGetPosition,
+                    requestCreatePosition,
+                    iupd,
+                    ibanTestUpdate,
+                    companyName
+                )
+            )
+        verify(gpdClient, Mockito.times(0)).createDebtPosition(any(), any(), anyOrNull())
+        verify(gpdClient, Mockito.times(0))
+            .invalidateDebtPosition(requestCreatePosition.paFiscalCode, iupd.value())
+        assertEquals(
+            ibanTestUpdate,
+            paymentPositionModelDtoCaptor.firstValue.paymentOption?.get(0)?.transfer?.get(0)?.iban
+        )
+    }
+
+    @Test
+    fun `update position (no iban) successfully`() = runTest {
+        val postalIbanUpdate: String = "IT99C0222211111000000000000"
+        val requestCreatePosition =
+            AcaTestUtils.createPositionRequestBody(iupd, 10, iban = null, postalIbanUpdate, true)
+        val responseGetPosition =
+            AcaTestUtils.responseGetPosition(
+                iupd,
+                10,
+                ibanTest,
+                PaymentPositionModelBaseResponseDto.StatusEnum.DRAFT
+            )
+        val responseCreate = AcaTestUtils.debitPositionModelResponse(iupd)
+        /* preconditions */
+        given(gpdClient.getDebtPosition(any(), any(), anyOrNull()))
+            .willReturn(Mono.just(responseGetPosition))
+        given(ibansClient.getIban(any(), any()))
+            .willReturn(Mono.just(Pair(ibanTestUpdate, companyName)))
+        given(
+                gpdClient.updateDebtPosition(
+                    any(),
+                    any(),
+                    paymentPositionModelDtoCaptor.capture(),
+                    anyOrNull()
+                )
+            )
+            .willReturn(Mono.just(responseCreate))
+        /* tests */
+        acaService.handleDebitPosition(requestCreatePosition)
+        /* Asserts */
+        verify(gpdClient, Mockito.times(1))
+            .getDebtPosition(requestCreatePosition.paFiscalCode, iupd.value())
+        verify(ibansClient, Mockito.times(1))
+            .getIban(eq(requestCreatePosition.paFiscalCode), anyOrNull())
+        verify(gpdClient, Mockito.times(1))
+            .updateDebtPosition(
+                requestCreatePosition.paFiscalCode,
+                iupd.value(),
+                acaUtils.updateOldDebitPositionObject(
+                    responseGetPosition,
+                    requestCreatePosition,
+                    iupd,
+                    ibanTestUpdate,
+                    companyName,
+                    postalIbanUpdate
                 )
             )
         verify(gpdClient, Mockito.times(0)).createDebtPosition(any(), any(), anyOrNull())
