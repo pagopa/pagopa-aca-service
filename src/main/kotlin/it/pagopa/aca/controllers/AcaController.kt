20ca6c6571199627221d1e455bca33ed78db6352
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-08-29 14:57:42 +0200

    [PAGOPA-2059] feat: Add check seg_codes authorization

--- src/main/kotlin/it/pagopa/aca/controllers/AcaController.kt
+++ src/main/kotlin/it/pagopa/aca/controllers/AcaController.kt
@@ -10,6 +10,7 @@ import java.time.Duration
 import java.time.temporal.ChronoUnit
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
@@ -21,11 +22,21 @@ class AcaController(private val webClient: WebClient = WebClient.create()) : PaC
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     override suspend fun newDebtPosition(
-        newDebtPositionRequestDto: NewDebtPositionRequestDto
+        newDebtPositionRequestDto: NewDebtPositionRequestDto,
+        segregationCodes: String?
     ): ResponseEntity<Unit> {
         logger.info("[ACA service] paCreatePosition")
+        // Check authorization
+        if (!checkAuth(segregationCodes, newDebtPositionRequestDto.iuv))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        // Handle request
         acaService.handleDebitPosition(newDebtPositionRequestDto)
         return ResponseEntity.ok().build()
+    }
+
+    // Fun to check authorization based on IUV prefix
+    private fun checkAuth(segregationCodes: String?, iuv: String): Boolean {
+        return segregationCodes?.contains(iuv.substring(0, 2)) == true
     }
 
     /** Controller warm up function, used to send a POST pa create position request */
